12fec21266fe577b3c35d06f0523541cf451fd07
maguzzz <markusm.touray@gmail.com>
2022-10-07 06:51:23 +0200

    user input done

--- AnimeByMood.py
+++ AnimeByMood.py
@@ -5,3 +5,8 @@ genres = [
     ["Mystery", "c"],
     ["Slice of life", "d"]
 ]
+print("----------------------------------------")
+print("What type of Anime Genre do you want ?\n")
+#gets input ands saves it in "input"
+input = input("Action | Chill | Mystery | Slice of life  : ")
+print("---------------------------------------- \n")
