8876a32bb9a0e8e30726b94689f0525ce27e1c19
maguzzz <markusm.touray@gmail.com>
2022-10-07 06:50:48 +0200

    List of genres made

---
+++ AnimeByMood.py
@@ -0,0 +1,7 @@
+#List of animes (First list items is the Genre)
+genres = [
+    ["Action", "Hunter × Hunterr", "JoJo’s Bizarre Adventure",],
+    ["Chill", "b"],
+    ["Mystery", "c"],
+    ["Slice of life", "d"]
+]
