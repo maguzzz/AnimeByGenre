3cc08277f1b57bfe33821becd17d9243fe9f82d4
maguzzz <markusm.touray@gmail.com>
2022-10-07 07:42:59 +0200

    ReadmeFile  and commenting

---
+++ AnimeByGenre.py
@@ -0,0 +1,22 @@
+# List of animes (First list items is the Genre)
+genres = [
+    ["Action", "Hunter × Hunterr", "Fire Force", "Akame ga Kill"],
+    ["Chill", "My Neighbor Totoro", "Girls'Last Tour", "Komi can’t communicate"],
+    ["Mystery", "Great Pretender", "Death Note", "Black Butler"],
+    ["Comedy", "The Devil as a Part-Timer", "Bloodline Blockade", "Blood Lad"],
+]
+print("----------------------------------------")
+print("What type of Anime Genre do you want ?\n")
+# Gets input and saves it in "input"
+input = input("Action | Chill | Mystery | Comedy  : ")
+print("---------------------------------------- \n")
+# Getting every genre comparing it with the input and printing the animes
+# If the Genre is not found an Error message will print
+for i in genres:
+    if i[0] == input:
+        print(input + " Animes: ", end="")
+        print(*i[1:], sep=", ")
+        break
+else:
+    print("Looks like we dont have the Genre " + input + "（ ；´Д｀）")
+print("----------------------------------------")
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Anime-by-mood-Python
+# AnimeByGenre
+
+Namespace = camelCase
+
+What i have used.
+
+-input module | to get user input 
+-for loops | to go through the lists
+-if else statement | to see wich animes should be recommended
+-2d list  | to store anime data
+-sep | to seperate list items with a ","
+-end | to get two printing commands in the same line
